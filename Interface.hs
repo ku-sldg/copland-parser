a11dc13fef8adb9e38cee614a84d2b6b830efb80
Will Thomas <30wthomas@gmail.com>
2022-05-26 14:05:22 -0500

    Adding interface and docs

---
+++ Interface.hs
@@ -0,0 +1,31 @@
+module CoplandTools where
+import CoplandLang (COPLAND)
+import Utils (transCop_Coq_IO, printCoq)
+import CoplandParser (parseCop)
+import CoplandQC (out_test, checkParser)
+
+----- Utilities -----
+translate_Cop_Coq :: String -> IO ()
+translate_Cop_Coq = transCop_Coq_IO
+
+parse_to_AST :: String -> COPLAND
+parse_to_AST = parseCop
+
+print_AST :: COPLAND -> String
+print_AST x = case (printCoq x) of
+                Just x -> x
+                Nothing -> error $ "error failed to print"
+
+
+----- Testing -----
+
+-- Creates an example copland phrase and translates it to Coq
+--      Arg 1 : String  := controls output file
+--      Arg 2 : Nat     := controls the complexity of the generated phrase
+generate_example_coq :: String -> Int -> IO ()
+generate_example_coq = out_test
+
+-- Runs QuickCheck on the parser, generating copland than ensuring it parses
+--      Arg 1 : Nat     := controls how many tests to run
+check_parser :: Int -> IO ()
+check_parser = checkParser 
--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # copland-parser
 General purpose Copland parser
+
+A good place to start is [Interface.hs], it has the most useful functions
+
+# Notes
+This parser uses Right Associativity for the Branch Operators. This is not currently in-line with the Copland Tutorial, but without Right Associativity, we must enforce explicit parenthesization around all branch phrases to ensure that we do not create ambiguity.
